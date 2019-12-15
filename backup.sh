d3c6874cc41ea4bf312c73c3a0770c8b5851dcf9
Mathias Aschhoff <web@little-devil.de>
2019-12-15 19:58:14 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
+#Require to intall apt-get install pv and to install https://github.com/Drewsif/PiShrink
+
 #!/bin/bash
 echo "Backup Raspberry"
 sudo dd bs=4M if=/dev/mmcblk0 | pv -s 8G | dd of=raspbian.img 
